d94197292deee540b9987734bf3b0a5534d76206
AmazingDoor <roystondaniel1@gmail.com>
2023-11-09 21:17:09 -0500

    Added Thumbnail.png

--- src/main/java/net/dpcoffee/coffeemod/mixin/ModEntityDataSaverMixin.java
+++ src/main/java/net/dpcoffee/coffeemod/mixin/ModEntityDataSaverMixin.java
@@ -1,18 +1,10 @@
 package net.dpcoffee.coffeemod.mixin;
 
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityType;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
-import net.minecraft.nbt.NbtElement;
-import net.minecraft.nbt.NbtList;
-import net.minecraft.sound.SoundEvent;
-import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
-import org.lwjgl.system.CallbackI;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
@@ -20,10 +12,6 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Map;
 
 @Mixin(LivingEntity.class)
@@ -38,13 +26,6 @@ public abstract class ModEntityDataSaverMixin {
     @Unique
     @Nullable
     private Float set_health = null;
-
-
-    @Inject(method = "writeCustomDataToNbt", at = @At("HEAD"))
-    public void writeCustom(NbtCompound nbt, CallbackInfo info) {
-
-
-    }
 
     @Inject(method = "readCustomDataFromNbt", at = @At("HEAD"))
     public void readCustom(NbtCompound nbt, CallbackInfo info) {
